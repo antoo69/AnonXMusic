7b379188a85ff3c59a1650db32f6328b35b9c9a7
antoo69 <antobarry69@gmail.com>
2025-09-13 13:32:50 +0700

    Update play.py
    Signed-off-by: antoo69 <antobarry69@gmail.com>

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -1,3 +1,4 @@
+import asyncio
 import random
 import string
 
@@ -664,6 +665,7 @@ async def slider_queries(client, CallbackQuery, _):
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
         )
+
 
 
 
